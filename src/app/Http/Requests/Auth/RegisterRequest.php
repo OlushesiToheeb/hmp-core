0d67b4a3be422eacb168c5d6fa4498cf39fbdb91
olushesitoheeb <olaolushesi98@gmail.com>
2023-05-12 08:07:48 +0100

    register user and assign role

---
+++ src/app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
+            'password_confirmation' => 'required',
+            'phone' => 'required|unique:users|min:11|max:11',
+            'user_type' => 'required'
+        ];
+    }
+}
